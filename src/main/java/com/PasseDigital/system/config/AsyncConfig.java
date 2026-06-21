2e272436567d6e51a0fcbf806a1948fa18218f3b
kaua <kaua.santanaj@gmail.com>
2026-06-21 18:33:00 -0300

    adding asyncConfiguration class

---
+++ src/main/java/com/PasseDigital/system/config/AsyncConfig.java
@@ -0,0 +1,28 @@
+package com.PasseDigital.system.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "emailExecutor")
+    public Executor emailExecutor(){
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(8);
+        executor.setMaxPoolSize(12);
+        executor.setQueueCapacity(300);
+        executor.setThreadNamePrefix("Email-");
+
+        return executor;
+
+    }
+}
